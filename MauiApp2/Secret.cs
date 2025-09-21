f76382865be944259fe03080ac9e66dfb9b9a86c
luiz0ar <luiz.rangel@alunos.unis.edu.br>
2025-09-21 18:32:48 -0300

    refactor: Remoção de 'using directives' desnecessários de Secret.cs

--- MauiApp2/Secret.cs
+++ MauiApp2/Secret.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MauiApp2;
+﻿namespace MauiApp2;
 
 internal static class Secrets
 {
